b1987f9323c4211c05a49a5640e63180cf49de27
Moustafa Hussein <mustafahussein1228@gmail.com>
2025-12-27 20:26:04 +0200

    Add image asset constants for app logo

---
+++ lib/core/constants/images.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAppLogo
+  /// assets/images/app_logo.png
+  static const String assetsImagesAppLogo = "assets/images/app_logo.png";
+}
+
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:interview_task_app/core/constants/images.dart';
+import 'package:interview_task_app/core/routes/app_routes.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToOnboarding();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+
+            child: Image.asset(
+              Assets.assetsImagesAppLogo,
+              width: 200,
+              height: 200,
+            ),
+          ),
+        ),
+        SizedBox(height: 30),
+        Text("Bookly App", style: Theme.of(context).textTheme.bodyLarge),
+      ],
+    );
+  }
+
+  void navigateToOnboarding() async {
+    await Future.delayed(Duration(seconds: 3), () {
+      if (!mounted) return;
+      GoRouter.of(context).pushNamed(AppRoutes.kOnBoardingView);
+    });
   }
 }
